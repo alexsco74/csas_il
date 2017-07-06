7b7c83cdd642692c3d69290f124cd57dc5deb2d8
sas <alexsco74@gmail.com>
2017-07-06 09:32:04 +0300

    add :visible settings

--- csas_il/csas_il.js
+++ csas_il/csas_il.js
@@ -2,8 +2,13 @@
     Drupal.behaviors.csasIl = {
         attach: function (context, settings) {
             function csasIl() {
+                var useVis = Drupal.settings.csas_il.useVis;
                 var botWinY = $(window).scrollTop() + $(window).height();
-                $('img.csas-il[data-src]:not(.acsas-img-not-loa):visible', context).each(function () {
+                var imgSel = 'img.csas-il[data-src]:not(.acsas-img-not-loa)';
+                if (useVis) {
+                    imgSel = 'img.csas-il[data-src]:not(.acsas-img-not-loa):visible';
+                }
+                $(imgSel).each(function () {
                     if ($(this).offset().top <= botWinY) {
                         var thisSrc = $(this).attr('data-src');
                         $(this).removeAttr('data-src');
@@ -13,40 +18,25 @@
                         $(this).attr('src', thisSrc);
                     }
                 });
-                //$('body').removeClass('csas-il-process');
             }
 
             function csasIlEv(tCon) {
-
                 $('img.acsas-img-not-loa.csas-il[data-src]:visible', tCon).each(function () {
-
                     var thisSrc = $(this).attr('data-src');
                     $(this).removeAttr('data-src');
-                    //$(this).bind('load', function () {
                     $(this).addClass('csas-il-ldd');
-                    //});
                     $(this).attr('src', thisSrc);
 
                 });
-
-                //$('body').removeClass('csas-il-process');
             }
 
-
             function csasIlEvVis(tCon) {
-
                 $('img.csas-il[data-src]', tCon).each(function () {
-
                     var thisSrc = $(this).attr('data-src');
                     $(this).removeAttr('data-src');
-                    //$(this).bind('load', function () {
                     $(this).addClass('csas-il-ldd');
-                    //});
                     $(this).attr('src', thisSrc);
-
                 });
-
-                //$('body').removeClass('csas-il-process');
             }
 
             $(window).load(function () {
@@ -55,32 +45,23 @@
                     csasIl();
                 });
             });
-
             $(window).resize(function () {
                 if ($('img[data-src]', document).length) {
                     csasIl();
                 }
             });
-
-            csasIl();
-
+            // csasIl();
             $(document).on('documentChange DOMNodeInserted', function () {
-
                 if ($('img[data-src]', document).length) {
-
                     csasIl();
                 }
             });
-
             $(document).on('acsasDivDisplay', function (e) {
                 csasIlEv($(e.target));
             });
-
             $(document).on('acsasVisibled', function (e) {
                 csasIlEvVis($(e.target));
             });
         }
     };
-
-
 })(jQuery);
